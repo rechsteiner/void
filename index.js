7fce1fd866f6f5448c37ac07ef674be868bfcfa5
Alexander Vanvik <alexander.vanvik@gmail.com>
2021-03-17 22:52:54 +0100

    Update frontend

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import("./pkg/static_void.js").then((lib) => {
   let editor = document.getElementById("editor");
+  let runButton = document.getElementById("run-button");
   let game = new lib.Game();
 
-  editor.addEventListener("input", function () {
+  runButton.addEventListener("click", function () {
     console.log(editor.value);
     game.change_program(editor.value);
   });
+
+  function animate() {
+    game.next_simulation_step();
+    requestAnimationFrame(() => animate());
+  }
+
+  animate();
 });
