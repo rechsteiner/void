7274579050ab871b5bddf737ee4d0796cffb6dfd
Martin Rechsteiner <marrechsteiner@gmail.com>
2021-08-12 22:03:19 +0200

    Add TypeScript compilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require("path");
+
 module.exports = {
-  entry: "./index.js",
+  entry: "./index.ts",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
   },
   mode: "development",
   experiments: {
