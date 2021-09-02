71ce0acfd01238e33c3f41584832ac252218c48b
Martin Rechsteiner <marrechsteiner@gmail.com>
2021-09-02 21:45:49 +0200

    Remove PhysicsWorldParameters struct
    Having two concepts of a world is just confusing.

--- src/systems/simulation.rs
+++ src/systems/simulation.rs
@@ -14,7 +14,13 @@ use rapier2d::{
 pub struct SimulationSystem {
     spaceship_handle: Option<RigidBodyHandle>,
     physics_pipeline: PhysicsPipeline,
-    physics_world: PhysicsWorldParameters,
+    gravity: Vector2<f32>,
+    integration_parameters: IntegrationParameters,
+    broad_phase: BroadPhase,
+    narrow_phase: NarrowPhase,
+    bodies: RigidBodySet,
+    colliders: ColliderSet,
+    joints: JointSet,
 }
 
 impl SimulationSystem {
@@ -22,15 +28,13 @@ impl SimulationSystem {
         SimulationSystem {
             spaceship_handle: None,
             physics_pipeline: PhysicsPipeline::new(),
-            physics_world: PhysicsWorldParameters {
-                gravity: Vector2::new(0.0, 100.0),
-                integration_parameters: IntegrationParameters::default(),
-                broad_phase: BroadPhase::new(),
-                narrow_phase: NarrowPhase::new(),
-                bodies: RigidBodySet::new(),
-                colliders: ColliderSet::new(),
-                joints: JointSet::new(),
-            },
+            gravity: Vector2::new(0.0, 100.0),
+            integration_parameters: IntegrationParameters::default(),
+            broad_phase: BroadPhase::new(),
+            narrow_phase: NarrowPhase::new(),
+            bodies: RigidBodySet::new(),
+            colliders: ColliderSet::new(),
+            joints: JointSet::new(),
         }
     }
 }
@@ -40,7 +44,7 @@ impl System for SimulationSystem {
         // TODO: For now we are just checking weather the bodies are empty and
         // so we only insert our components once. This won't work when we start
         // adding and removing components.
-        if self.physics_world.bodies.len() == 0 {
+        if self.bodies.len() == 0 {
             for (index, (rigid_body, shape, physics_mode)) in world
                 .query3::<RigidBody, Shape, PhysicsMode>()
                 .iter()
@@ -59,7 +63,7 @@ impl System for SimulationSystem {
                     .mass(rigid_body.mass)
                     .build();
 
-                let entity_handle = self.physics_world.bodies.insert(entity_rb);
+                let entity_handle = self.bodies.insert(entity_rb);
 
                 if index == 0 {
                     self.spaceship_handle = Some(entity_handle);
@@ -72,20 +76,13 @@ impl System for SimulationSystem {
                 }
 
                 let entity_collider = ColliderBuilder::convex_hull(&points).unwrap().build();
-                self.physics_world.colliders.insert(
-                    entity_collider,
-                    entity_handle,
-                    &mut self.physics_world.bodies,
-                );
+                self.colliders
+                    .insert(entity_collider, entity_handle, &mut self.bodies);
             }
         }
 
         // TODO: Apply force on a specfic body with the correct vector
-        let spaceship_body = self
-            .physics_world
-            .bodies
-            .get_mut(self.spaceship_handle.unwrap())
-            .unwrap();
+        let spaceship_body = self.bodies.get_mut(self.spaceship_handle.unwrap()).unwrap();
 
         for command in world.commands.iter() {
             match command {
@@ -107,18 +104,18 @@ impl System for SimulationSystem {
         }
 
         self.physics_pipeline.step(
-            &self.physics_world.gravity,
-            &self.physics_world.integration_parameters,
-            &mut self.physics_world.broad_phase,
-            &mut self.physics_world.narrow_phase,
-            &mut self.physics_world.bodies,
-            &mut self.physics_world.colliders,
-            &mut self.physics_world.joints,
+            &self.gravity,
+            &self.integration_parameters,
+            &mut self.broad_phase,
+            &mut self.narrow_phase,
+            &mut self.bodies,
+            &mut self.colliders,
+            &mut self.joints,
             &(),
             &(),
         );
 
-        let physics_bodies = self.physics_world.bodies.iter();
+        let physics_bodies = self.bodies.iter();
         let rigid_bodies = world.query_mut::<RigidBody>().into_iter();
 
         for ((_handle, physics_body), rigid_body) in physics_bodies.zip(rigid_bodies) {
@@ -136,14 +133,4 @@ impl System for SimulationSystem {
             rigid_body.angular_velocity = physics_body.angvel();
         }
     }
-}
-
-struct PhysicsWorldParameters {
-    pub gravity: Vector2<f32>,
-    pub integration_parameters: IntegrationParameters,
-    pub broad_phase: BroadPhase,
-    pub narrow_phase: NarrowPhase,
-    pub bodies: RigidBodySet,
-    pub colliders: ColliderSet,
-    pub joints: JointSet,
 }
